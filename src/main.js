29291067e73ac83f163fde08401bb2b08841f14f
ankys <ankysp@gmail.com>
2020-12-01 22:19:08 +0900

    add nav

--- src/main.js
+++ src/main.js
@@ -3,6 +3,39 @@
 function defined(obj) {
 	return obj !== undefined;
 }
+function DomClearChildren(element) {
+	var range = document.createRange();
+	range.selectNodeContents(element);
+	range.deleteContents();
+}
+function DomCreateElement(document, parent, tagName, attributes) {
+	attributes = attributes || {};
+	var element = document.createElement(tagName);
+	for (var key in attributes) {
+		var value = attributes[key];
+		if (key == "text") {
+			element.appendChild(document.createTextNode(value));
+		} else if (key == "style") {
+			for (var key2 in value) {
+				var value2 = value[key2];
+				element.style.setProperty(key2, value2);
+			}
+		} else {
+			element.setAttribute(key, value);
+		}
+	}
+	if (parent !== undefined && parent !== null) {
+		parent.appendChild(element);
+	}
+	return element;
+}
+function DomCreateText(document, parent, text) {
+	var node = document.createTextNode(text);
+	if (parent !== undefined && parent !== null) {
+		parent.appendChild(node);
+	}
+	return node;
+}
 
 // modify at mark
 var es = document.getElementsByClassName("fa-at");
@@ -12,7 +45,9 @@ es.forEach(function(e) {
 });
 
 // navigation
-var url = "directory.txt";
+var ePathDirectory = document.getElementById("path-directory");
+var pathDirectory = ePathDirectory.textContent;
+var url = pathDirectory;
 var xhr = new XMLHttpRequest();
 xhr.addEventListener("load", function(event) {
 	var text = xhr.responseText;
@@ -25,17 +60,24 @@ xhr.addEventListener("load", function(event) {
 		var item = items[path];
 		var parent = items[item.parent];
 		if (defined(parent)) {
-			parent.children.push(item);
+			parent.children.push(path);
 		}
 	}
 	console.log(items);
-	var eNav = document.getElementById("main-nav");
+	var eNav = document.getElementById("main-nav2");
 	var eNavInfo = document.getElementById("main-nav-info");
-	var parents = eNavInfo.textContent.split(">");
+	var parents = eNavInfo.textContent.split(" > ");
 	parents.forEach(function(path) {
-		console.log(items[path].title);
+		var item = items[path];
+		DomCreateElement(document, eNav, "a", { text: item.title || path, href: path, description: item.description });
 		if (items[path].children.length > 0) {
-			console.log(">");
+			DomCreateText(document, eNav, " ");
+			var e = DomCreateElement(document, eNav, "a", { text: ">", href: "javascript:void(0);" });
+			e.addEventListener("click", function(event) {
+				var children = item.children;
+				var block = DomCreateElement(document, eNav, "div", { text: "aaaa", style: { display: "inline-block", position: "absolute", left: event.x, top: event.y } });
+			});
+			DomCreateText(document, eNav, " ");
 		}
 	});
 });
